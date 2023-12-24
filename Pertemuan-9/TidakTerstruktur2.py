c2f2737ac93716d82585ea5a64da00896aae2e62
Muhammad Putra Rizki Julianto <139139725+MptrJ1807@users.noreply.github.com>
2023-12-24 19:40:47 +0700

    Add files via upload

---
+++ Pertemuan-9/TidakTerstruktur2.py
@@ -0,0 +1,15 @@
+print("konversi suhu Fahrenheit")
+
+#Entry
+suhu = input("Masukkan suhu dalam Fahrenheit")
+
+#rumus
+C = (5/9 * (float(suhu) - 32))
+R = (4/9 * (float(suhu) - 32))
+K = (5/9 * (float(suhu) - 32)) + 273
+
+#print
+print(suhu + "Fahrenheit sama dengan")
+print(str(C) + "Celcius")
+print(str(R) + "Reamur")
+print(str(K) + "Kelvin")
